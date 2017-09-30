8415a9154e432703b99701b970ad58581b925734
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-30 23:13:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,9 @@
 def reformat_languages(languages)
   reformatted_has = {}
-  
+
   languages.each do |type, language_hash|
-    language_hash.each do |language_name, 
+    language_hash.each do |language_data|
+      puts language_data
+    end
+  end
 end
