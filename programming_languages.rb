1c8d52164509f4d6d90545d9e6ade5678548a385
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-01 02:09:19 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  reformatted_hash = {}
+  languages.each do |type, language_hash|
+    language_hash.each do |language_name, data|
+      data.each do |data_name, value|
+        if reformatted_hash.key?(language_name) == false
+          reformatted_hash[language_name] = {data_name => value, :style => [type]}
+        else
+          reformatted_hash[language_name][:style] << type
+        end
+      end
+    end
+  end
+  reformatted_hash
 end
