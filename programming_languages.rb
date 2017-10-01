acaa17318ed0386b1877d4712f850eaf990f0f4d
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-01 02:09:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,14 @@
 def reformat_languages(languages)
   reformatted_hash = {}
-  merged_hash = nil
   languages.each do |type, language_hash|
     language_hash.each do |language_name, data|
       data.each do |data_name, value|
-        hash = {data_name => value}
-        another_hash = {:style => [type]}
-        merged_hash = hash.merge(another_hash)
-        #puts merged_hash
+        if reformatted_hash.key?(language_name) == false
+          reformatted_hash[language_name] = {data_name => value, :style => [type]}
+        else
+          reformatted_hash[language_name][:style] << type
+        end
       end
-      reformatted_hash[language_name] = merged_hash
     end
   end
   reformatted_hash
